f5e86330a8cbd92adca36324d6467049454f4cad
Ariya Hidayat <ariya.hidayat@gmail.com>
2024-07-31 23:03:21 -0700

    Use searx.work

--- gamal.js
+++ gamal.js
@@ -12,7 +12,7 @@ const LLM_API_BASE_URL = process.env.LLM_API_BASE_URL || 'https://openrouter.ai/
 const LLM_CHAT_MODEL = process.env.LLM_CHAT_MODEL || 'meta-llama/llama-3-8b-instruct';
 const LLM_STREAMING = process.env.LLM_STREAMING !== 'no';
 
-const SEARXNG_URL = process.env.SEARXNG_URL || 'https://searxng.online';
+const SEARXNG_URL = process.env.SEARXNG_URL || 'https://searx.work';
 const JINA_API_KEY = process.env.JINA_API_KEY;
 const BRAVE_SEARCH_API_KEY = process.env.BRAVE_SEARCH_API_KEY;
 const TOP_K = 3;
