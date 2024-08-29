ce65ca713b1ebd443be21e7a3285333a1ee736b4
Simon <sstur@me.com>
2024-08-29 08:17:17 -0700

    Add one comment

--- src/support/node-builtins.js
+++ src/support/node-builtins.js
@@ -1,3 +1,7 @@
+/**
+ * The sole purpose of this module is to use require() to import Node
+ * built-in modules but expose them via ES Modules so Rollup is happy.
+ */
 const fs = require('fs');
 const http = require('http');
 const readline = require('readline');
