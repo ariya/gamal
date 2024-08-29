d9736adfdf4846eb7aeed785ec654e16ad1b6bda
Simon <sstur@me.com>
2024-08-29 11:36:24 -0700

    Better types for pipe.js

--- src/support/pipe.js
+++ src/support/pipe.js
@@ -1,11 +1,24 @@
 /**
- * Creates a new function by chaining multiple async functions from left to right.
- *
- * @param  {...any} fns - Functions to chain
+ * @template I, O
+ * @typedef {(input: I) => Promise<O>} Fn
  */
-export function pipe(...fns) {
-    /**
-     * @param {any} arg
-     */
+
+/**
+ * @param {...Fn<any, any>} fns
+ * @returns {(input: any) => Promise<any>}
+ */
+const createPipeline = (...fns) => {
     return (arg) => fns.reduce((d, fn) => d.then(fn), Promise.resolve(arg));
-}
+};
+
+/**
+ * This is a better typed version of createPipeline above.
+ *
+ * @type {{
+ *   <A, R>(...fns: [Fn<A, R>]): (input: A) => Promise<R>
+ *   <A, B, R>(...fns: [Fn<A, B>, Fn<B, R>]): (input: A) => Promise<R>
+ *   <A, B, C, R>(...fns: [Fn<A, B>, Fn<B, C>, Fn<C, R>]): (input: A) => Promise<R>
+ *   <A, B, C, D, R>(...fns: [Fn<A, B>, Fn<B, C>, Fn<C, D>, Fn<D, R>]): (input: A) => Promise<R>
+ * }}
+ */
+export const pipe = createPipeline;
